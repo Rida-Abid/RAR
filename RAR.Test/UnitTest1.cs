6fdd2cca1bc385cbdd4b4d636b8a3e0edce7cb9d
ridaa <ridaa@DESKTOP-LLRRUIU>
2022-11-24 22:39:07 +0500

    Add project files.

---
+++ RAR.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Xunit.Sdk;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RAR.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ RAR/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace rar;
+{
+
+
+
+    public class program
+    {
+        public static string Main(string[] args)
+        {
+
+            List<string> Words = new List<string>();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Please enter a word");
+                var input = Console.ReadLine();
+                Words.Add(input);
+            }
+            
+            
+
+            foreach (var item in Words)
+            {
+                return (item);
+            }
+
+            program sort = new program();
+
+            Words.Sort();
+            foreach (var item in Words)
+            {
+                return item;
+            }
+
+            program reverse = new program();
+
+            Words.Reverse();
+            foreach (var item in Words)
+            {
+                 return item;
+            }
+
+                    //Words.OrderBy(x => x);
+
+            
+
+            
+        
+    }
+}
